34bf207546b6f2cdd424ab9519e079a0c5439a23
FabioRigamonti <65313023+FabioRigamonti@users.noreply.github.com>
2024-03-11 14:25:55 +0100

    Add files via upload

---
+++ tests/example.py
@@ -0,0 +1,43 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+from pyADfit import read_config_and_launch
+
+def read_data(input_path):
+    """
+    Reads in data from a text file. USER DEFINED.
+
+    Parameters
+    ----------
+    input_path : str
+        Path to the input file.
+
+    Returns
+    -------
+    xdata : np.ndarray
+        Array of x-data.
+    ydata : np.ndarray
+        Array of y-data.
+    yerr : np.ndarray
+        Array of y-errors.
+
+    """
+    xdata,ydata,yerr = np.loadtxt(input_path,unpack=True,usecols=[0,1,2])
+
+    indsort = np.argsort(xdata)
+    xdata = xdata[indsort]
+    ydata = ydata[indsort]
+    yerr = yerr[indsort]
+
+    xdata = 1e15*xdata 
+    ydata = 1e45*ydata
+    yerr = 1e45*yerr
+
+    return xdata,ydata,yerr
+
+if __name__=='__main__':
+    config_path = './config.yaml'
+
+    post_df,best = read_config_and_launch(config_path,read_data)
+
+    plt.show()
